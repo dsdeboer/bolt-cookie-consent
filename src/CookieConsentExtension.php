280ef0b3f7a6555f802a87972e5a6242acbde04f
Dustin Utecht <dustin.utecht@codm.de>
2019-01-29 14:34:12 +0100

    updated to version 3, load the css without cdn

--- src/CookieConsentExtension.php
+++ src/CookieConsentExtension.php
@@ -2,6 +2,7 @@
 
 namespace Bolt\Extension\Leskis\CookieConsent;
 
+use Bolt\Asset\File\Stylesheet;
 use Bolt\Asset\Snippet\Snippet;
 use Bolt\Asset\File\JavaScript;
 use Bolt\Controller\Zone;
@@ -22,7 +23,10 @@ class CookieConsentExtension extends SimpleExtension
             'learnMore'  => 'More info',
             'link'       => '',
             'container'  => '',
-            'theme'      => 'light-floating',
+            'theme'      => 'block',
+            'position'   => 'bottom',
+            'palette-popup-background' => '#383b75',
+            'palette-button-background' => '#f1d600',
             'path'       => '/',
             'domain'     => '',
             'expiryDays' => 365
@@ -33,7 +37,8 @@ class CookieConsentExtension extends SimpleExtension
     {
         return [
             (new JavaScript('cookieconsent.min.js'))->setZone(Zone::FRONTEND)->setLate(true)->setPriority(998),
-            (new Snippet())->setCallback([$this, 'cookieConsentSnippet'])->setZone(Zone::FRONTEND)->setLocation(Target::BEFORE_BODY_JS)->setPriority(997),
+            (new Stylesheet('cookieconsent.min.css'))->setZone(Zone::FRONTEND)->setLate(true)->setPriority(997),
+            (new Snippet())->setCallback([$this, 'cookieConsentSnippet'])->setZone(Zone::FRONTEND)->setLocation(Target::AFTER_BODY_JS)->setPriority(996),
         ];
     }
 
@@ -43,40 +48,83 @@ class CookieConsentExtension extends SimpleExtension
 
         $html = <<< EOM
 <script>
-window.cookieconsent_options = {
-    message: '%message%',
-    dismiss: '%dismiss%',
-    learnMore: '%learnMore%',
-    link: %link%,
-    container: %container%,
-    theme: '%theme%',
-    path: '%path%',
-    domain: '%domain%',
-    expiryDays: %expiryDays%
-};
+window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+        "showLink": '%learnMore%',
+        "position": '%position%',
+        "container": %container%,
+        "theme": '%theme%',
+        "content": {
+            "message": '%message%',
+            "dismiss": '%dismiss%',
+            "link": %link%
+        },
+        "cookie": {
+            "path": '%path%',
+            "domain": '%domain%',
+            "expiryDays": %expiryDays%
+        },
+        "palette": {
+            "popup": {
+                "background": "%palette_popup_background%"
+            },
+            "button": {
+                "background": "%palette_button_background%"
+            }
+        }
+    })
+});
 </script>
 EOM;
-        $html = str_replace("%message%", htmlspecialchars($config['message'], ENT_QUOTES, "UTF-8"), $html);
-        $html = str_replace("%dismiss%", htmlspecialchars($config['dismiss'], ENT_QUOTES, "UTF-8"), $html);
-        $html = str_replace("%learnMore%", htmlspecialchars($config['learnMore'], ENT_QUOTES, "UTF-8"), $html);
+        $searchArray = [
+            '%message%',
+            '%dismiss%',
+            '%learnMore%',
+            '%position%',
+            '%palette_popup_background%',
+            '%palette_button_background%'
+        ];
 
-        if ($config['link'] != "") {
-            $html = str_replace("%link%", "'" . htmlspecialchars($config['link'], ENT_QUOTES, "UTF-8") . "'", $html);
+        $replaceArray = [
+            htmlspecialchars($config['message'], ENT_QUOTES),
+            htmlspecialchars($config['dismiss'], ENT_QUOTES),
+            htmlspecialchars($config['learnMore'], ENT_QUOTES),
+            htmlspecialchars($config['position'], ENT_QUOTES),
+            htmlspecialchars($config['palette-popup-background'], ENT_QUOTES),
+            htmlspecialchars($config['palette-button-background'], ENT_QUOTES),
+        ];
+
+        if ($config['link'] !== '') {
+            $searchArray[] = '%link%';
+            $replaceArray[] = "'" . htmlspecialchars($config['link'], ENT_QUOTES) . "'";
         } else {
-            $html = str_replace("%link%", 'null', $html);
+            $searchArray[] = '%link%';
+            $replaceArray[] = 'null';
         }
 
-        if ($config['container'] != "") {
-            $html = str_replace("%container%", "'" . htmlspecialchars($config['container'], ENT_QUOTES, "UTF-8") . "'", $html);
+        if ($config['container'] !== '') {
+            $searchArray[] = '%container%';
+            $replaceArray[] = "'" . htmlspecialchars($config['container'], ENT_QUOTES) . "'";
         } else {
-            $html = str_replace("%container%", 'null', $html);
+            $searchArray[] = '%container%';
+            $replaceArray[] = 'null';
         }
 
-        $html = str_replace("%theme%", htmlspecialchars($config['theme'], ENT_QUOTES, "UTF-8"), $html);
-        $html = str_replace("%path%", htmlspecialchars($config['path'], ENT_QUOTES, "UTF-8"), $html);
-        $html = str_replace("%domain%", htmlspecialchars($config['domain'], ENT_QUOTES, "UTF-8"), $html);
-        $html = str_replace("%expiryDays%", $config['expiryDays'], $html);
+        $searchArray = array_merge($searchArray, [
+            '%theme%',
+            '%path%',
+            '%domain%',
+            '%expiryDays%'
+        ]);
 
+        $replaceArray = array_merge($replaceArray, [
+            htmlspecialchars($config['theme'], ENT_QUOTES),
+            htmlspecialchars($config['path'], ENT_QUOTES),
+            htmlspecialchars($config['domain'], ENT_QUOTES),
+            $config['expiryDays']
+        ]);
+
+        $html = str_replace($searchArray, $replaceArray, $html);
         return new \Twig_Markup($html, 'UTF-8');
     }
 }
